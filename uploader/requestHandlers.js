1d9c42c4b857ded8f71e5cca3e95cb11492c3442
J. Guo <jijing.guo@gmail.com>
2012-03-01 22:46:19 +0800

    add router and requestHandler modules

--- uploader/requestHandlers.js
+++ uploader/requestHandlers.js
@@ -5,12 +5,23 @@
  * This module, requestHanlers, is used to handle all the requsts passed by router object.
  */
 
-function start() {
+var exec = require("child_process").exec;
+
+function start(response) {
     console.log("Request handler 'start' was called.");
+
+    exec("dir c:\\ /s/a", { timeout:10000, maxBuffer:20000 * 1024 }, function (error, stdout, stderr) {
+        response.writeHead(200, {"Content-Type":"text/plain"});
+        response.write(stdout);
+        response.end();
+    });
 }
 
-function upload() {
+function upload(response) {
     console.log("Request handler 'upload' was called.");
+    response.writeHead(200, {"Content-Type":"text/plain"});
+    response.write("Hello Upload");
+    response.end();
 }
 
 exports.start = start;
--- uploader/server.js
+++ uploader/server.js
@@ -1,19 +1,15 @@
 var http = require("http");
 var url = require("url");
 
-function start(router, handle) {
-    http.createServer(
-        function (request, response) {
-            var pathname = url.parse(request.url).pathname;
-            console.log("Request for " + pathname + " received.");
+function start(route, handle) {
+    function onRequest(request, response) {
+        var pathname = url.parse(request.url).pathname;
+        console.log("Request for " + pathname + " received.");
 
-            router(handle, pathname);
+        route(handle, pathname, response);
+    }
 
-            response.writeHead(200, {"Content-Type":"text/plain"});
-            response.write("Hello World");
-            response.end();
-        }
-    ).listen(8888);
+    http.createServer(onRequest).listen(8888);
     console.log("Server has started.");
 }
 
