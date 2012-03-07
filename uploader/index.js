0280afb046ff70bc803d18556bc3e12447f52e1a
J. Guo <jijing.guo@gmail.com>
2012-03-07 14:53:09 +0800

    add router and requestHandler modules

--- uploader/index.js
+++ uploader/index.js
@@ -1,4 +1,4 @@
-var server = require("./server.js");
+var server = require("./server");
 var router = require("./router");
 var requestHandlers = require("./requestHandlers");
 
@@ -6,6 +6,7 @@ var handle = {};
 handle["/"] = requestHandlers.start;
 handle["/start"] = requestHandlers.start;
 handle["/upload"] = requestHandlers.upload;
+handle["/show"] = requestHandlers.show;
 
 
 server.start(router.route, handle);
--- uploader/requestHandlers.js
+++ uploader/requestHandlers.js
@@ -5,9 +5,11 @@
  * This module, requestHanlers, is used to handle all the requsts passed by router object.
  */
 
-var querystring = require("querystring");
+var querystring = require("querystring"),
+    fs = require("fs"),
+    formidable = require("formidable");
 
-function start(response, postData) {
+function start(response) {
     console.log("Request handler 'start' was called.");
 
     var body = '<html>'+
@@ -16,9 +18,10 @@ function start(response, postData) {
         'charset=UTF-8" />'+
         '</head>'+
         '<body>'+
-        '<form action="/upload" method="post">'+
-        '<textarea name="text" rows="20" cols="60"></textarea>'+
-        '<input type="submit" value="Submit text" />'+
+        '<form action="/upload" enctype="multipart/form-data" '+
+        'method="post">'+
+        '<input type="file" name="upload" multiple="multiple">'+
+        '<input type="submit" value="Upload file" />'+
         '</form>'+
         '</body>'+
         '</html>';
@@ -28,13 +31,36 @@ function start(response, postData) {
     response.end();
 }
 
-function upload(response, postData) {
+function upload(response, request) {
     console.log("Request handler 'upload' was called.");
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("You've sent the text: "+
-        querystring.parse(postData).text);
-    response.end();
+
+    var form = new formidable.IncomingForm();
+    console.log("about to parse");
+    form.parse(request, function(error, fields, files) {
+        console.log("parsing done");
+        fs.renameSync(files.upload.path, "c:\\test.jpg");
+        response.writeHead(200, {"Content-Type": "text/html"});
+        response.write("received image:<br/>");
+        response.write("<img src='/show' />");
+        response.end();
+    });
+}
+
+function show(response) {
+    console.log("Request handler 'show' was called.");
+    fs.readFile("c:\\test.jpg", "binary", function(error, file) {
+        if(error) {
+            response.writeHead(500, {"Content-Type": "text/plain"});
+            response.write(error + "\n");
+            response.end();
+        } else {
+            response.writeHead(200, {"Content-Type": "image/png"});
+            response.write(file, "binary");
+            response.end();
+        }
+    });
 }
 
 exports.start = start;
 exports.upload = upload;
+exports.show = show;
